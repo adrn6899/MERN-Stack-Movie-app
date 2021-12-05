f6be7cd471af811ece488ce8393b29edbdb4c772
adrn6899 <aguilaradrian423@gmail.com>
2021-12-06 02:28:53 +0800

    commit

--- index.js
+++ index.js
@@ -20,14 +20,44 @@ app.use(cookieParser());
 app.post('/api/users/register',(req, res)=>{
     const user = new User(req.body)
 
-    user.save((err,userData)=>{
-        if(err) return json({success:false, err})
-    })
-    return res.status(200).json({success:true})
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false, err});
+        res.status(200).json({
+            success:true,
+            userData: doc
+        })
+    }) 
 })
 
-app.get('/',(req, res)=>{
-    res.send('running on port 5000')
-});
+app.post('/api/user/login',(req, res)=>{
+    //finding the email in the database
+    User.findOne({email: req.body.email},(err,user)=>{
+        if(user)
+        return res.json({
+            loginSuccess: false,
+            message: "Auth failed, email not found"
+        });
+        //compare the password
+        user.comparePassword(req.body.password,(err, isMatch)=>{
+            if(!isMatch){
+                return res.json ({loginSuccess: false, message: "password mismatch"})
+            }
+        })
+        //generate the token
+        user.generateToken((err,user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        })
+    })
+     
+})
+
+// app.get('/',(req, res)=>{
+//     res.send('running on port 5000')
+// });
 
 app.listen(5000);
