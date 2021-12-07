f6e0b7b0be74bf82d2901566ae8c1fe470c32b3d
adrn6899 <aguilaradrian423@gmail.com>
2021-12-08 01:19:02 +0800

    reducer setup completed

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,20 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducer from './reducers';
+
+const createStoreMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>,
+  <Provider store={createStoreMiddleware(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
